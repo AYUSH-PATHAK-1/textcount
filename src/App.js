22f0aa583c18ae827d20cda4dd75df85897f02e5
AYUSH-PATHAK-1 <developerasp7@gmail.com>
2023-07-28 18:23:56 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [textdata,setTextdata]=useState("");
+  const [predata,setPredata]=useState("");
+  const cleardata=()=>{
+    setTextdata("");
+  }
+  const handleData=(e)=>{
+    setTextdata(e.target.value);
+  }
+  const previewdata=()=>{
+    setPredata(textdata);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container my-5">
+      <textarea cols="30" rows="10" className="w-100" onChange={handleData} value={textdata}>{textdata}</textarea>
+      <button className="btn btn-dark my-3" onClick={cleardata}>Clear Data</button>
+      <p>{predata}</p>
+      <button className="btn btn-success" onClick={previewdata}>Preview Data</button>
+      <p className="text-center text-danger">{textdata.length} characters are present inside</p>
+      <p className="text-center text-success">{textdata.split(" ").length} words are present inside</p>
     </div>
-  );
+    
+    </>
+  )
 }
 
-export default App;
+export default App
